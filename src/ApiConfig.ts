7bac5d84d329d7fe34fe7289b83fdcdef029c5c3
Yurii Orlyk <aigletter@gmail.com>
2025-10-08 00:44:42 +0200

    added interface for token store

--- src/ApiConfig.ts
+++ src/ApiConfig.ts
@@ -3,13 +3,19 @@ import {Token} from "./Token";
 import {StoreType} from "./Stores";
 import {AxiosRequestConfig} from "axios";
 
+export interface TokenStore {
+    get(): Token | null;
+    set(token: Token): void;
+    forget(): void;
+}
+
 export interface AuthConfig {
     method: string,
     route: string,
     notAuthRedirect: () => void,
 
     stateless?: {
-        tokenStore: StoreType|ExpirableStore|string,
+        tokenStore: StoreType|TokenStore|string,
         formatToken: (raw: unknown) => Token;
     } | undefined,
     stateful?: {
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { provideApiConfig, useApi } from "./Api";
-export type { ApiConfig, AuthConfig } from "./ApiConfig";
+export type { ApiConfig, AuthConfig, TokenStore } from "./ApiConfig";
 export type { Token } from "./Token";
 export { StoreType } from "./Stores";
