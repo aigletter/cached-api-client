763a076bbc3ce3afeb14b1cc0febcf1f7cc6cb00
Yurii Orlyk <aigletter@gmail.com>
2025-11-20 03:27:40 +0100

    export TokenStoreAdapter

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ export type { Api } from "./Api";
 export { provideApiConfig, useApi } from "./Api";
 export type { ApiConfig, AuthConfig, TokenStore } from "./Config";
 export type { Token } from "./Token";
-export { StoreType } from "./Stores";
+export { StoreType, TokenStoreAdapter } from "./Stores";
